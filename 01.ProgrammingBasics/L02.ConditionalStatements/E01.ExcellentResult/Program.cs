74702ef643b5c399916c7e6bb207de852bcabaad
koss054 <konstantin054@abv.bg>
2022-08-25 19:52:04 +0300

    Upload: Conditional Statements

---
+++ 01.ProgrammingBasics/L02.ConditionalStatements/E01.ExcellentResult/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace E01.ExcellentResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score = double.Parse(Console.ReadLine());
+            const double excellentResult = 5.50;
+
+            if (score >= excellentResult)
+                Console.WriteLine("Excellent!");
+        }
+    }
+}
