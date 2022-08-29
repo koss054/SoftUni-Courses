985acf81a3d10856dea1fc42fd0287fce5cdb6ad
koss054 <konstantin054@abv.bg>
2022-08-29 22:52:44 +0300

    Upload: Second Exercise
    Uploading one by one as I'm currently working on a Game Project, that is too large atm and I want to make contributions.

---
+++ 01.ProgrammingBasics/L02.ConditionalStatements/E02.GreaterNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace E02.GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+                Console.WriteLine(firstNumber);
+            else
+                Console.WriteLine(secondNumber);
+        }
+    }
+}
