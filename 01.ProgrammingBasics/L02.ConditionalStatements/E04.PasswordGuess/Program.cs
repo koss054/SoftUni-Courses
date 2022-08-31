ef7bf436750f9b37e8b34063fe3dda34d047fbfe
koss054 <konstantin054@abv.bg>
2022-08-31 16:36:41 +0300

    Upload: Exercises

---
+++ 01.ProgrammingBasics/L02.ConditionalStatements/E04.PasswordGuess/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace E04.PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string realPassword = "s3cr3t!P@ssw0rd";
+            string userInput = Console.ReadLine();
+
+            if (userInput == realPassword)
+                Console.WriteLine("Welcome");
+            else
+                Console.WriteLine("Wrong password!");
+        }
+    }
+}
