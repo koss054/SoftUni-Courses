b8aa482c1b93ce806ce6ae27f2f138d5fb1f137e
koss054 <konstantin054@abv.bg>
2022-10-06 18:00:48 +0300

    Update: Seed the Database

--- 07.ASP.NETFundamentals/E08.WorkshopForumApp/ForumApp/Data/ForumAppDbContext.cs
+++ 07.ASP.NETFundamentals/E08.WorkshopForumApp/ForumApp/Data/ForumAppDbContext.cs
@@ -6,6 +6,13 @@
 
     public class ForumAppDbContext : DbContext
     {
+        private Post FirstPost { get; set; }
+
+        private Post SecondPost { get; set; }
+
+        private Post ThirdPost { get; set; }
+
+
         public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
             : base(options)
         {
@@ -16,9 +23,34 @@
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            SeedPosts();
             builder.Entity<Post>();
 
             base.OnModelCreating(builder);
+        }
+
+        private void SeedPosts()
+        {
+            this.FirstPost = new Post()
+            {
+                Id = 1,
+                Title = "First Post",
+                Content = "Content for the first post. Very interesting and informative, for sure."
+            };
+
+            this.SecondPost = new Post()
+            {
+                Id = 2,
+                Title = "How to Cook Better",
+                Content = "An age old question. How do you do it? Some say watch cookings shows. But it's most effective to just do it. Sponsored by Nike."
+            };
+
+            this.ThirdPost = new Post()
+            {
+                Id = 3,
+                Title = "A Third Post??",
+                Content = "How is this possible? Incredible! A third post on this formum."
+            };
         }
     }
 }
