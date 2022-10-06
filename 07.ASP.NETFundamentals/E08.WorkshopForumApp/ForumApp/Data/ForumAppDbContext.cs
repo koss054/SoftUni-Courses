c7c2077b1c0157b7ffac13fff9880fb153418857
koss054 <konstantin054@abv.bg>
2022-10-06 17:55:47 +0300

    Upload: Defining the DbContext

--- 07.ASP.NETFundamentals/E08.WorkshopForumApp/ForumApp/Data/ForumAppDbContext.cs
+++ 07.ASP.NETFundamentals/E08.WorkshopForumApp/ForumApp/Data/ForumAppDbContext.cs
@@ -2,8 +2,23 @@
 {
     using Microsoft.EntityFrameworkCore;
 
+    using Entities;
+
     public class ForumAppDbContext : DbContext
     {
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
 
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Post>();
+
+            base.OnModelCreating(builder);
+        }
     }
 }
