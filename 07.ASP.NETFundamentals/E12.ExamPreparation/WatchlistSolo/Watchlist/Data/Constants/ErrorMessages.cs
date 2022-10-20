e94452330e331a1ac1ae10dbba950a68639e7667
koss054 <konstantin054@abv.bg>
2022-10-20 19:47:21 +0300

    Upload: User Controller and Views
    The user controller and the views for it have been created successfully. The user can now register, login and logout.
    
    The register and login can be accessed by logged out users, while the logout can be only accessed by logged in users.

---
+++ 07.ASP.NETFundamentals/E12.ExamPreparation/WatchlistSolo/Watchlist/Data/Constants/ErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Watchlist.Data.Constants
+{
+    public static class ErrorMessages
+    {
+        // User
+        public const string InvalidUser = "Invalid user details!";
+    }
+}
---
+++ 07.ASP.NETFundamentals/E12.ExamPreparation/WatchlistSolo/Watchlist/Models/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Watchlist.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Constants.DataConstants.User;
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(MaxUserNameLength, MinimumLength = MinUserNameLength)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(MaxEmailLength, MinimumLength = MinEmailLength)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(MaxPasswordLength, MinimumLength = MinPasswordLength)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
