b18db7dbe62cd603a87fe7542db5bc1564a6a53b
koss054 <konstantin054@abv.bg>
2022-10-21 13:22:10 +0300

    Update: HomeController.cs
    When a user is logged in and they try to go to the home page, they now get redirected to All, from the MoviesController.

--- 07.ASP.NETFundamentals/E12.ExamPreparation/WatchlistSolo_v2/Watchlist/Controllers/HomeController.cs
+++ 07.ASP.NETFundamentals/E12.ExamPreparation/WatchlistSolo_v2/Watchlist/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
-namespace Watchlist.Controllers
+﻿namespace Watchlist.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Movies");
+            }
+
             return View();
         }
     }
