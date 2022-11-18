23bef7306dda45fb063ee919fa62df5994063dd3
koss054 <konstantin054@abv.bg>
2022-11-18 17:51:00 +0200

    Add data constants and entities

---
+++ 08.ASP.NETAdvanced/Data/Entities/Agent.cs
@@ -0,0 +1,25 @@
+﻿namespace HouseRenting.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using Microsoft.AspNetCore.Identity;
+
+    using static Data.DataConstants.Agent;
+
+    public class Agent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(MaxPhoneLength)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser? User { get; set; }
+    }
+}
