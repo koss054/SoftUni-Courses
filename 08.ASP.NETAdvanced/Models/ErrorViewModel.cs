4d74c136e807931dfdf0b74b65942ade8b9c6120
koss054 <konstantin054@abv.bg>
2022-11-18 17:29:41 +0200

    Add initial house renting files

---
+++ 08.ASP.NETAdvanced/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseRenting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
