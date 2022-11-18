2b4dfcf48fd9626415a6e411008e162f3f056ccb
koss054 <konstantin054@abv.bg>
2022-11-18 18:31:04 +0200

    Update app context

---
+++ 08.ASP.NETAdvanced/Data/HouseRentingDbContext.cs
@@ -0,0 +1,39 @@
+﻿namespace HouseRenting.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using Data.Entities;
+
+    public class HouseRentingDbContext : IdentityDbContext
+    {
+        public HouseRentingDbContext(DbContextOptions<HouseRentingDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
+
+        public DbSet<House> Houses { get; init; } = null!;
+
+        public DbSet<Category> Categories { get; init; } = null!;
+
+        public DbSet<Agent> Agents { get; init; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<House>()
+                .HasOne(h => h.Category)
+                .WithMany(c => c.Houses)
+                .HasForeignKey(h => h.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<House>()
+                .HasOne(h => h.Agent)
+                .WithMany()
+                .HasForeignKey(h => h.AgentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
