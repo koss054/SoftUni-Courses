5a65a9e19fd56ecb61e613877acb34a4074da6cd
koss054 <konstantin054@abv.bg>
2022-11-20 23:07:58 +0200

    Add ClaimsPrincipalExtensions

---
+++ 08.ASP.NETAdvanced/E05.Workshop/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace HouseRenting.Infrastructure
+{
+    using System.Security.Claims;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
