fa1cc870c4db8fadbb3c12338daf49cdb0c0d74d
koss054 <konstantin054@abv.bg>
2022-11-20 23:19:48 +0200

    Add become agent functionality to vieew model and controller

--- 08.ASP.NETAdvanced/E05.Workshop/Controllers/AgentController.cs
+++ 08.ASP.NETAdvanced/E05.Workshop/Controllers/AgentController.cs
@@ -4,12 +4,26 @@
     using Microsoft.AspNetCore.Mvc;
 
     using Models.Agents;
+    using Services.Agents;
+    using Infrastructure;
 
     public class AgentController : Controller
     {
+        private readonly IAgentService agentService;
+
+        public AgentController(IAgentService _agentService)
+        {
+            agentService = _agentService;
+        }
+
         [Authorize]
         public IActionResult Become()
         {
+            if (this.agentService.ExistsById(this.User.Id()))
+            {
+                return BadRequest();
+            }
+
             return View();
         }
 
@@ -17,6 +31,30 @@
         [Authorize]
         public IActionResult Become(BecomeAgentFormModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userId = this.User.Id();
+            if (this.agentService.ExistsById(userId))
+            {
+                return BadRequest();
+            }
+            
+            if (this.agentService.UserWithPhoneNumberExists(model.PhoneNumber))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber),
+                    "Phone number already exists. Enter another one.");
+            }
+
+            if (this.agentService.UserHasRents(userId))
+            {
+                ModelState.AddModelError("Error",
+                    "You should have no rents to become an agent.");
+            }
+
+            this.agentService.Create(userId, model.PhoneNumber);
             return RedirectToAction(nameof(HousesController.All), "Houses");
         }
     }
--- 08.ASP.NETAdvanced/E05.Workshop/Models/Agents/BecomeAgentFormModel.cs
+++ 08.ASP.NETAdvanced/E05.Workshop/Models/Agents/BecomeAgentFormModel.cs
@@ -1,6 +1,15 @@
 ﻿namespace HouseRenting.Models.Agents
 {
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.DataConstants.Agent;
+
     public class BecomeAgentFormModel
     {
+        [Required]
+        [StringLength(MaxPhoneLength, MinimumLength = MinPhoneLength)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
     }
 }
