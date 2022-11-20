302c65db34d6f2ee4954ae363a9275fb654c2cd5
koss054 <konstantin054@abv.bg>
2022-11-20 23:12:43 +0200

    Add additional agent services

--- 08.ASP.NETAdvanced/E05.Workshop/Services/Agents/AgentService.cs
+++ 08.ASP.NETAdvanced/E05.Workshop/Services/Agents/AgentService.cs
@@ -1,6 +1,7 @@
 ﻿namespace HouseRenting.Services.Agents
 {
     using Data;
+    using Data.Entities;
 
     public class AgentService : IAgentService
     {
@@ -15,6 +16,30 @@
         {
             return this.data.Agents
                 .Any(a => a.UserId == userId);
+        }
+
+        public bool UserWithPhoneNumberExists(string phoneNumber)
+        {
+            return this.data.Agents
+                .Any(a => a.PhoneNumber == phoneNumber);
+        }
+
+        public bool UserHasRents(string userId)
+        {
+            return this.data.Houses
+                .Any(h => h.RenterId == userId);
+        }
+
+        public void Create(string userId, string phoneNumber)
+        {
+            var agent = new Agent()
+            {
+                UserId = userId,
+                PhoneNumber = phoneNumber
+            };
+
+            this.data.Agents.Add(agent);
+            this.data.SaveChanges();
         }
     }
 }
--- 08.ASP.NETAdvanced/E05.Workshop/Services/Agents/IAgentService.cs
+++ 08.ASP.NETAdvanced/E05.Workshop/Services/Agents/IAgentService.cs
@@ -3,5 +3,11 @@
     public interface IAgentService
     {
         bool ExistsById(string userId);
+
+        bool UserWithPhoneNumberExists(string phoneNumber);
+
+        bool UserHasRents(string userId);
+
+        void Create(string userId, string phoneNumber);
     }
 }
