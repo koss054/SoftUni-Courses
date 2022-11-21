ab734caedec7a5f30b854f89cd8bcdcadea14b1c
koss054 <konstantin054@abv.bg>
2022-11-21 20:52:04 +0200

    Add additional service models for houses

---
+++ 08.ASP.NETAdvanced/E05.Workshop/Services/Houses/Models/HouseServiceModel.cs
@@ -0,0 +1,22 @@
+﻿namespace HouseRenting.Services.Houses.Models
+{
+    using System.ComponentModel;
+
+    public class HouseServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Address { get; set; }
+
+        [DisplayName("Image URL")]
+        public string ImageUrl { get; set; }
+
+        [DisplayName("Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+        [DisplayName("It's Rented")]
+        public bool IsRented { get; set; }
+    }
+}
