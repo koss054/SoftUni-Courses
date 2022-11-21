8f201101d888463fdcbb2a6a2263905ccb9614f6
koss054 <konstantin054@abv.bg>
2022-11-21 21:25:08 +0200

    Add properties to AllHouseQueryModel

--- 08.ASP.NETAdvanced/E05.Workshop/Models/Houses/AllHousesQueryModel.cs
+++ 08.ASP.NETAdvanced/E05.Workshop/Models/Houses/AllHousesQueryModel.cs
@@ -1,6 +1,27 @@
 ﻿namespace HouseRenting.Models.Houses
 {
+    using System.ComponentModel.DataAnnotations;
+
+    using Services.Houses.Models;
+
     public class AllHousesQueryModel
     {
+        public const int HousesPerPage = 3;
+
+        public string Category { get; init; }
+
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; init; }
+
+        public HouseSorting Sorting { get; init; }
+
+        public int CurrentPage { get; init; } = 1;
+
+        public int TotalHousesCount { get; set; }
+
+        public IEnumerable<string> Categories { get; set; }
+
+        public IEnumerable<HouseServiceModel> Houses { get; set; }
+            = new List<HouseServiceModel>();
     }
 }
