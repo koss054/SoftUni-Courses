33b0a833a18b6f20e097e5efed7d2ecc932ba22c
koss054 <konstantin054@abv.bg>
2022-11-21 22:25:03 +0200

    Add properties to HouseDetailsViewModel

--- 08.ASP.NETAdvanced/E05.Workshop/Models/Houses/HouseDetailsViewModel.cs
+++ 08.ASP.NETAdvanced/E05.Workshop/Models/Houses/HouseDetailsViewModel.cs
@@ -2,5 +2,12 @@
 {
     public class HouseDetailsViewModel
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Address { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
