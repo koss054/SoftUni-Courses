f4789be7739143f6273d1892013aa4af73e57d2c
koss054 <konstantin054@abv.bg>
2022-11-22 23:44:28 +0200

    Add FullName to AgentServiceModel

--- 08.ASP.NETAdvanced/E10.Workshop/Services/Agents/Models/AgentServiceModel.cs
+++ 08.ASP.NETAdvanced/E10.Workshop/Services/Agents/Models/AgentServiceModel.cs
@@ -2,6 +2,8 @@
 {
     public class AgentServiceModel
     {
+        public string FullName  { get; init; }
+
         public string PhoneNumber { get; init; }
 
         public string Email { get; init; }
