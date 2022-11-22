0e9460f39a7bc788db7ae781b0d4768e8491db99
koss054 <konstantin054@abv.bg>
2022-11-22 17:24:21 +0200

    Add StatisticsService to Services

---
+++ 08.ASP.NETAdvanced/E10.Workshop/Services/Statistics/StatisticsService.cs
@@ -0,0 +1,28 @@
+﻿namespace HouseRenting.Services.Statistics
+{
+    using Data;
+    using Models;
+
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly HouseRentingDbContext data;
+
+        public StatisticsService(HouseRentingDbContext _data)
+        {
+            data = _data;
+        }
+
+        public StatisticsServiceModel Total()
+        {
+            var totalHouses = this.data.Houses.Count();
+            var totalRents = this.data.Houses
+                .Where(h => h.RenterId != null).Count();
+
+            return new StatisticsServiceModel
+            {
+                TotalHouses = totalHouses,
+                TotalRents = totalRents
+            };
+        }
+    }
+}
