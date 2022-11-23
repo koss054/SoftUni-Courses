591f1af35da0a0ee0e507f511fec81ee1081b934
koss054 <konstantin054@abv.bg>
2022-11-23 12:15:29 +0200

    Create AdminConstants

---
+++ 08.ASP.NETAdvanced/E10.Workshop/AdminConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRenting
+{
+    public class AdminConstants
+    {
+        public const string AdminRoleName = "Administrator";
+        public const string AdminEmail = "admin@mail.bg";
+    }
+}
