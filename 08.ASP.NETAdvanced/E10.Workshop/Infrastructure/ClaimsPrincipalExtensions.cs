c8e4279d57bfb084d3f7774909c648719e0d12e9
koss054 <konstantin054@abv.bg>
2022-11-23 12:47:41 +0200

    Add IsAdmin to ClaimsPrincipalExtensions

--- 08.ASP.NETAdvanced/E10.Workshop/Infrastructure/ClaimsPrincipalExtensions.cs
+++ 08.ASP.NETAdvanced/E10.Workshop/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -2,11 +2,18 @@
 {
     using System.Security.Claims;
 
+    using static AdminConstants;
+
     public static class ClaimsPrincipalExtensions
     {
         public static string Id(this ClaimsPrincipal user)
         {
             return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
         }
     }
 }
