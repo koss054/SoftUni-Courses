22206de7a1ddc45b75724657a34a7a9f7d5bbd6f
koss054 <konstantin054@abv.bg>
2023-09-11 18:03:55 +0300

    E03.Queue - Solution

--- 12.DataStructuresFundamentals/E04.LinearDataStructures/Problem03.Queue/Queue.cs
+++ 12.DataStructuresFundamentals/E04.LinearDataStructures/Problem03.Queue/Queue.cs
@@ -8,41 +8,105 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                Element = element;
+                Next = next;
+            }
+
+            public Node(T element)
+                : this(element, null)
+            {
+            }
         }
 
-        private Node head;
+        private Node _head;
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if (_head == null)
+            {
+                _head = new Node(item);
+                Count++;
+                return;
+            }
+
+            var node = _head;
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            node.Next = new Node(item);
+            Count++;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            EnsureNotEmpty();
+
+            if (_head.Next == null)
+            {
+                var headElement = _head.Element;
+                _head = null;
+                Count--;
+                return headElement;
+            }
+
+            var oldHeadElement = _head.Element;
+            var newHead = _head.Next;
+
+            _head = newHead;
+            Count--;
+            return oldHeadElement;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            EnsureNotEmpty();
+            return _head.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var node = _head;
+
+            while (node != null)
+            {
+                if (node.Element.Equals(item))
+                {
+                    return true;
+                }
+
+                node = node.Next;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = _head;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        void EnsureNotEmpty()
         {
-            throw new NotImplementedException();
+            if (_head == null)
+                throw new InvalidOperationException();
         }
     }
 }
