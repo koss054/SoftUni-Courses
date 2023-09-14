fd36701a855335469abaa13305e4d59f707f253c
koss054 <konstantin054@abv.bg>
2023-09-14 17:39:47 +0300

    E05.ReverseNumbersWithStack - Solution

---
+++ 12.DataStructuresFundamentals/E05.LinearDataStructuresExercise/05.ReverseNumbersWithStack/Program.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+string input = Console.ReadLine();
+
+if (input.Length == 0)
+{
+    Console.WriteLine("(empty)");
+    return;
+}
+
+string[] stringNumbers = input.Split(" ");
+Stack<int> numbers = new Stack<int>();
+
+foreach (string stringNumber in stringNumbers)
+    numbers.Push(int.Parse(stringNumber));
+
+while (numbers.Count > 0)
+    Console.Write($"{numbers.Pop()} ");
+
+Console.WriteLine();
