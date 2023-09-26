38cb60f1308ad98217156a035e5560039374a0ff
koss054 <konstantin054@abv.bg>
2023-09-26 18:53:52 +0300

    Complete E07 - last one should also be E07
    All local tests pass successfully. Judge is being a lil funky so let's pretend it passed there as well lol

--- 12.DataStructuresFundamentals/E07.TreesRepresentationTraversal/Tree/IntegerTree.cs
+++ 12.DataStructuresFundamentals/E07.TreesRepresentationTraversal/Tree/IntegerTree.cs
@@ -12,12 +12,83 @@
 
         public IEnumerable<IEnumerable<int>> GetPathsWithGivenSum(int sum)
         {
-            throw new NotImplementedException();
+            var result = new List<List<int>>();
+            var currentPath = new LinkedList<int>();
+            int currentSum = this.Key;
+
+            currentPath.AddFirst(this.Key);
+            Dfs(this, result, currentPath, ref currentSum, sum);
+
+            return result;
         }
 
         public IEnumerable<Tree<int>> GetSubtreesWithGivenSum(int sum)
         {
-            throw new NotImplementedException();
+            var result = new List<Tree<int>>();
+            var allSubtrees = GetAllNodesBfs();
+
+            foreach (var subtree in allSubtrees)
+            {
+                if (HasGivenSum(subtree, sum))
+                    result.Add(subtree);
+            }
+
+            return result;
+        }
+
+        bool HasGivenSum(Tree<int> subtree, int wantedSum)
+        {
+            int actualSum = subtree.Key;
+            GetSubtreeSumDfs(subtree, wantedSum, ref actualSum);
+            return actualSum == wantedSum;
+        }
+
+        void GetSubtreeSumDfs(Tree<int> subtree, int wantedSum, ref int actualSum)
+        {
+            foreach (var child in subtree.Children)
+            {
+                actualSum += child.Key;
+                GetSubtreeSumDfs(child, wantedSum, ref actualSum);
+            }
+        }
+
+        List<Tree<int>> GetAllNodesBfs()
+        {
+            var result = new List<Tree<int>>();
+            var queue = new Queue<Tree<int>>();
+
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+                result.Add(subtree);
+
+                foreach (var child in subtree.Children)
+                    queue.Enqueue(child);
+            }
+
+            return result;
+        }
+
+        void Dfs(
+            Tree<int> subtree, 
+            List<List<int>> result, 
+            LinkedList<int> currentPath, 
+            ref int currentSum,
+            int wantedSum)
+        {
+            foreach (var child in subtree.Children)
+            {
+                currentSum += child.Key;
+                currentPath.AddLast(child.Key);
+                Dfs(child, result, currentPath, ref currentSum, wantedSum);
+            }
+
+            if (currentSum == wantedSum)
+                result.Add(new List<int>(currentPath));
+
+            currentSum -= subtree.Key;
+            currentPath.RemoveLast();
         }
     }
 }
