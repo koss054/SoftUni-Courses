f17f5c238d5116d92e74bfc13b90552426237458
koss054 <konstantin054@abv.bg>
2023-09-30 21:13:49 +0300

    E08. Heaps, BST - 1st n 2nd Exercise

---
+++ 12.DataStructuresFundamentals/E08.HeapsBST/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,98 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
+        {
+            Value = element;
+            LeftChild = left;
+            RightChild = right;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            var sb = new StringBuilder();
+
+            PreOrderDfs(sb, indent, this);
+
+            return sb.ToString();
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            if (LeftChild != null)
+                LeftChild.ForEachInOrder(action);
+
+            action(this.Value);
+
+            if (RightChild != null)
+                RightChild.ForEachInOrder(action);
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> InOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (LeftChild != null)
+                result.AddRange(LeftChild.InOrder());
+
+            result.Add(this);
+
+            if (RightChild != null)
+                result.AddRange(RightChild.InOrder());
+
+            return result;
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (LeftChild != null)
+                result.AddRange(LeftChild.PostOrder());
+
+            if (RightChild != null)
+                result.AddRange(RightChild.PostOrder());
+
+            result.Add(this);
+
+            return result;
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            result.Add(this);
+
+            if (LeftChild != null)
+                result.AddRange(LeftChild.PreOrder());
+
+            if (RightChild != null)
+                result.AddRange(RightChild.PreOrder());
+
+            return result;
+        }
+
+        void PreOrderDfs(StringBuilder sb, int indent, IAbstractBinaryTree<T> binaryTree)
+        {
+            sb.Append(new String(' ', indent)).AppendLine(binaryTree.Value.ToString());
+
+            if (binaryTree.LeftChild != null)
+                PreOrderDfs(sb, indent + 2, binaryTree.LeftChild);
+
+            if (binaryTree.RightChild != null)
+                PreOrderDfs(sb, indent + 2, binaryTree.RightChild);
+        }
+    }
+}
---
+++ 12.DataStructuresFundamentals/E08.HeapsBST/02.BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,93 @@
+﻿namespace _02.BinarySearchTree
+{
+    using System;
+
+    public class BinarySearchTree<T> : IBinarySearchTree<T>
+        where T : IComparable<T>
+    {
+        Node _root;
+
+        public BinarySearchTree() { }
+
+        private BinarySearchTree(Node node)
+        {
+
+            _root = node;
+        }
+
+        public bool Contains(T element)
+            => FindNode(element) != null;
+
+        public void EachInOrder(Action<T> action)
+        {
+            EachInOrder(action, _root);
+        }
+
+        public void Insert(T element)
+        {
+            _root = Insert(element, _root);
+        }
+
+        public IBinarySearchTree<T> Search(T element)
+        {
+            var node = FindNode(element);
+
+            if (node is null) return null;
+
+            return new BinarySearchTree<T>(node);
+        }
+
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; private set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        private void EachInOrder(Action<T> action, Node node)
+        {
+            if (node is null) return;
+
+            EachInOrder(action, node.Left);
+            action.Invoke(node.Value);
+            EachInOrder(action, node.Right);
+        }
+
+        private Node Insert(T element, Node node)
+        {
+            if (node is null) node = new Node(element);
+            else if (element.CompareTo(node.Value) < 0) node.Left = Insert(element, node.Left);
+            else if (element.CompareTo(node.Value) > 0) node.Right = Insert(element, node.Right);
+
+            return node;
+        }
+
+        private Node FindNode(T element)
+        {
+            var node = _root;
+
+            while (node != null)
+            {
+                if (element.CompareTo(node.Value) < 0) node = node.Left;
+                else if (element.CompareTo(node.Value) > 0) node = node.Right;
+                else break;
+            }
+
+            return node;
+        }
+
+        private void PreOrderCopy(Node node)
+        {
+            if (node is null) return;
+
+            Insert(node.Value);
+            PreOrderCopy(node.Left);
+            PreOrderCopy(node.Right);
+        }
+    }
+}
