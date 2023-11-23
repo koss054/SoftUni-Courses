9c7fa80498cac9a5dcdd6ab9aab6fe549ddd3375
koss054 <konstantin054@abv.bg>
2023-11-23 11:18:07 +0200

    04 - Add initial files for this lesson

---
+++ 13.DataStructuresAdvanced/04.B-Trees_Trees_and_Red-Black_Exercise/01.RedBlackTree/RedBlackTree.cs
@@ -0,0 +1,98 @@
+﻿namespace _01.RedBlackTree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RedBlackTree<T>
+        : IBinarySearchTree<T> where T : IComparable
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public int Count { get; set; }
+        }
+
+        private Node root;
+
+        private RedBlackTree(Node node)
+        {
+        }
+
+        public RedBlackTree()
+        {
+        }    
+
+        public void Insert(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBinarySearchTree<T> Search(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Rank(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Select(int rank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Ceiling(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Floor(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Range(T startRange, T endRange)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
