a8bc9ffabce75fc3584a2dfba9fb84a167c3e073
koss054 <konstantin054@abv.bg>
2023-12-04 11:30:43 +0200

    Exam prep - Task Manager solution

--- 13.DataStructuresAdvanced/10.ExamPrep/Exam.TaskManager/TaskManager.cs
+++ 13.DataStructuresAdvanced/10.ExamPrep/Exam.TaskManager/TaskManager.cs
@@ -1,58 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Exam.TaskManager
 {
     public class TaskManager : ITaskManager
     {
+        private LinkedList<Task> _taskQueue = new LinkedList<Task>();
+        private HashSet<Task> _executedTasks = new HashSet<Task>();
+        private Dictionary<string, Task> _allTasks = new Dictionary<string, Task>();
+
         public void AddTask(Task task)
         {
-            throw new NotImplementedException();
+            _taskQueue.AddLast(task);
+            _allTasks.Add(task.Id, task);
         }
 
         public bool Contains(Task task)
         {
-            throw new NotImplementedException();
+            return _allTasks.ContainsKey(task.Id);
         }
 
         public void DeleteTask(string taskId)
         {
-            throw new NotImplementedException();
+            if (!_allTasks.ContainsKey(taskId))
+            {
+                throw new ArgumentException();
+
+            }
+
+            var task = _allTasks[taskId];
+            _allTasks.Remove(taskId);
+
+            if (_executedTasks.Contains(task))
+            {
+                _executedTasks.Remove(task);
+            }
+            else
+            {
+                _taskQueue.Remove(task);
+            }
         }
 
         public Task ExecuteTask()
         {
-            throw new NotImplementedException();
+            if (_taskQueue.Count == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            var task = _taskQueue.First.Value;
+            _taskQueue.RemoveFirst();
+
+            _executedTasks.Add(task);
+
+            return task;
         }
 
         public IEnumerable<Task> GetAllTasksOrderedByEETThenByName()
         {
-            throw new NotImplementedException();
+            return _allTasks
+                .Values
+                .OrderByDescending(x => x.EstimatedExecutionTime)
+                .ThenBy(x => x.Name);
+
         }
 
         public IEnumerable<Task> GetDomainTasks(string domain)
         {
-            throw new NotImplementedException();
+            var tasks = _taskQueue.Where(x => x.Domain == domain).ToList();
+
+            if (tasks.Count == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return tasks;
         }
 
         public Task GetTask(string taskId)
         {
-            throw new NotImplementedException();
+            if (!_allTasks.ContainsKey(taskId))
+            {
+                throw new ArgumentException();
+            }
+
+            return _allTasks[taskId];
         }
 
         public IEnumerable<Task> GetTasksInEETRange(int lowerBound, int upperBound)
         {
-            throw new NotImplementedException();
+            return _taskQueue
+                .Where(x => x.EstimatedExecutionTime >= lowerBound
+                         && x.EstimatedExecutionTime <= upperBound);
         }
 
         public void RescheduleTask(string taskId)
         {
-            throw new NotImplementedException();
+            if (!_allTasks.ContainsKey(taskId))
+            {
+                throw new ArgumentException();
+            }
+
+            var task = _allTasks[taskId];
+            _executedTasks.Remove(task);
+            _taskQueue.AddLast(task);
         }
 
         public int Size()
         {
-            throw new NotImplementedException();
+            return _allTasks.Count;
         }
     }
 }
